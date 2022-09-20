3b3fc621d43ffb97f9e7b5d99e55dbd8f01f6dce
diypcfev <username@email.com>
2022-09-20 14:32:36 +0800

    init

---
+++ src/main/java/com/example/testspringbootrubbish/TestSpringbootRubbishApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootrubbish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootRubbishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringbootRubbishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringbootrubbish/TestSpringbootRubbishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootrubbish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringbootRubbishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
